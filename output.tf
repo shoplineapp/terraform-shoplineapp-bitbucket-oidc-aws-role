1aea268cfd946a8ba466c8b8e08bc8796c35ab3c
Willis Chou <willis@shoplineapp.com>
2022-02-26 14:42:59 +0800

    feat(terraform) simplify by only support 1 role once

--- output.tf
+++ output.tf
@@ -1,9 +1,4 @@
-output "roles" {
-  description = "IAM roles created mapping."
-  value       = {
-    for role_key, role in aws_iam_role.this : role_key => {
-      name = role.name
-      arn  = role.arn
-    }
-  }
+output "role_arn" {
+  description = "IAM role created"
+  value       = aws_iam_role.this.arn
 }
