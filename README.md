338419b187d9ed9ae2dd485ad848cd61b79de74c
Mao-Lin Wang <focaaby@gmail.com>
2025-04-16 10:37:37 +0800

    Feature/cp 251 eks access entries iac (#6)
    * feat: support eks access entries iac 
    ---------
    
    Co-authored-by: Solomon Chen <solomon@shoplineapp.com>
    Co-authored-by: Solomon Chen <solomon.chen16@gmail.com>

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 Terraform module for create AWS IAM Role to support Bitbucket OIDC.
 
-### Contribution guidelines
+## Contribution guidelines
 
-1. Directly modify the code
+1. Directly modify the tf code.
 2. Push to feature branch
 3. Verify your changes with feature branch
-4. After you verified the changes, merge to dev.
-5. Once you decide to release the changes, merge dev to master and give it a tag!
+4. After you verified the changes, merge to master.
+5. Once you decide to release the changes, merge feature branch to master and give it a tag!
 6. Done, everyone can use your changes with new tag.
--- main.tf
+++ main.tf
@@ -31,3 +31,34 @@ resource "aws_iam_role_policy_attachment" "this" {
   role       = aws_iam_role.this.name
   policy_arn = var.policy_arns[count.index]
 }
+
+resource "aws_eks_access_entry" "this" {
+  count         = var.create_eks_access_entry == true ? 1 : 0
+  cluster_name  = var.eks_cluster_name
+  principal_arn = aws_iam_role.this.arn
+  # Use helm chart to create the group by default, please find the group name convention in helm chart repo.
+  # ref: https://github.com/shoplineapp/helm-charts/blob/master/eks/templates/role_admin.yaml
+  kubernetes_groups = var.eks_access_entry_scope == "namespace" ? [for ns in var.eks_cluster_namespaces : "group-${ns}-admin"] : null
+}
+
+resource "aws_eks_access_policy_association" "this" {
+  count         = var.create_eks_access_entry == true ? 1 : 0
+  cluster_name  = var.eks_cluster_name
+  principal_arn = aws_iam_role.this.arn
+  # Give the cd role namespace admin by default, ref: https://docs.aws.amazon.com/eks/latest/userguide/access-policy-permissions.html
+  policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAdminPolicy"
+  access_scope {
+    type       = var.eks_access_entry_scope
+    namespaces = var.eks_access_entry_scope == "namespace" ? var.eks_cluster_namespaces : null
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.33.0"
+      # aws_eks_access_policy_association required version > 5.33.0
+    }
+  }
+}
